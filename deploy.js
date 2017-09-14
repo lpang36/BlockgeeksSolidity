1531d3fa3369a47cf423df5354a4a53ff5e228a1
Lawrence Pang <lawrencepang36@gmail.com>
2017-09-14 12:32:23 -0400

    fix some bugs

--- deploy.js
+++ deploy.js
@@ -1,9 +1,9 @@
-var Web3 = require("Web3");
+var Web3 = require("./node_modules/web3");
 var solc = require("solc");
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 var acct1 = web3.eth.accounts[0];
 var source = `contract helloWorld {
-	string public message;
+	string public message; 
 	function helloWorld() {
 		message = "hello world";
 	}
@@ -13,13 +13,14 @@ var source = `contract helloWorld {
 }
 `
 var compiled = solc.compile(source);
-var data = compiled.contracts.helloWorld.bytecode;
-var api = JSON.parse(compiled.contracts.helloWorld.interface);
-var contract = web3.eth.contract(api);
+var data = compiled.contracts[':helloWorld'].bytecode;
+var abi = JSON.parse(compiled.contracts[':helloWorld'].interface);
+var contract = web3.eth.contract(abi);
 var deployed = contract.new({
 	from: acct1,
 	data: data,
-	gas: 10000; //test value
-	gasPrice: 1; //test value 
-},(error,contract)=>{})
+	gas: web3.eth.estimateGas({data:data}), 
+	gasPrice: 1 //test value 
+},(error,contract)=>{});
+//console.log(deployed);
 deployed.sayHi(); //test

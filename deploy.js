eee96691c02a38a07a7c6da072e9d7e734fa1446
Lawrence Pang <lawrencepang36@gmail.com>
2017-09-20 17:24:22 -0400

    create escrow contract

--- deploy.js
+++ deploy.js
@@ -2,12 +2,15 @@ var Web3 = require("./node_modules/web3");
 var solc = require("solc");
 var fs = require("fs");
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-function deploy(fn) {
-	var acct1 = web3.eth.accounts[0];
+var acct1 = web3.eth.accounts[0];
+var acct2 = web3.eth.accounts[1];
+var acct3 = web3.eth.accounts[2];
+var acct4 = web3.eth.accounts[3];
+function deploy(fn,cn) {
 	var source = fs.readFileSync("/home/lpang/Documents/GitHub/BlockgeeksSolidity/"+fn+".sol").toString();
 	var compiled = solc.compile(source);
-	var data = compiled.contracts[':'+fn].bytecode;
-	var abi = JSON.parse(compiled.contracts[':'+fn].interface);
+	var data = compiled.contracts[':'+cn].bytecode;
+	var abi = JSON.parse(compiled.contracts[':'+cn].interface);
 	var contract = web3.eth.contract(abi);
 	var deployed = contract.new({
 		from: acct1,
@@ -15,6 +18,8 @@ function deploy(fn) {
 		gas: web3.eth.estimateGas({data:data}), 
 		gasPrice: 1 //test value 
 	},(error,contract)=>{});
-	//setTimeout(function() {deployed.sayHi();},100); 
 	return deployed;
+}
+function deploy(fn) {
+	return deploy(fn,fn);
 }

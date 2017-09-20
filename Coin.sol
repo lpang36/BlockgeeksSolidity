95e7a678484f499adc38fc22b425ae5de3c9f51b
Lawrence Pang <lawrencepang36@gmail.com>
2017-09-20 17:33:52 -0400

    create coin

---
+++ Coin.sol
@@ -0,0 +1,40 @@
+pragma solidity 0.4.11;
+
+contract Coin {
+	address owner;
+	uint public totalSupply;
+	mapping(address=>uint) public balances;
+	function Coin(uint _supply)
+		owner = msg.sender;
+		totalSupply = _supply;
+		balances[owner] += _supply;
+	}
+	event Transfer(address indexed _to, address indexed _from, uint _value);
+	event newCoinLog(address _to, uint _amount, uint _newSupply);
+	modifier onlyOwner() {
+		if (msg.sender!=owner) {
+			throw;
+		} else {
+			_;
+		}
+	}
+	function getBalance(address _addr) constant returns (uint) {
+		return balances[_addr];
+	}
+	function transfer(address _to, uint _amount) returns (bool) {
+		if (balances[msg.sender]<_amount) throw;
+		balances[msg.sender] -= _amount;
+		balances[_to]+=_amount;
+		Transfer(_to, msg.sender,_amount);
+		return true;
+	}
+	function mint(uint _amount) onlyOwner returns (bool) {
+		totalSupply+=_amount;
+		balances[owner]+=_amount;
+		newCoinLog(owner,_amount,totalSupply);
+		return true;
+	}
+	function disable() onlyOwner {
+		selfdestruct(owner);
+	}
+}
